9bc4e5d9124bcb9d127f99010d2fe28a01b413ac
codeStackr <rdzaman20000@gmail.com>
2023-11-29 23:08:15 +0600

    Add schema codes,linters and models

---
+++ app/models/post.rb
@@ -0,0 +1,8 @@
+class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+
+  validates :title, presence: true
+  validates :text, presence: true
+end
---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  has_many :posts, foreign_key: 'author_id', dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+
+  validates :name, presence: true
+  validates :photo_url, presence: true, format: { with: %r{\Ahttps?://.*\z}, message: 'must be a valid URL' }
+  validates :bio, length: { maximum: 500 }
+end
