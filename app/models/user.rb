d516e3ae777e6310763075388dcfbe5c2c3e3391
codeStackr <rdzaman20000@gmail.com>
2023-12-01 01:03:54 +0600

    Fix error

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
 
   validates :name, presence: true
-  validates :photo_url, presence: true, format: { with: /\Ahttps?:\/\/.*\z/, message: "must be a valid URL" }
+  validates :photo_url, presence: true, format: { with: %r{\Ahttps?://.*\z}, message: 'must be a valid URL' }
   validates :bio, length: { maximum: 500 }
 
   def three_most_recent_posts
