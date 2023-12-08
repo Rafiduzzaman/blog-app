5624de7804b048f8c1b26cae0205a93836392d8c
codeStackr <rdzaman20000@gmail.com>
2023-12-08 09:24:14 +0600

    Add posts controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,11 @@
+# app/controllers/posts_controller.rb
+class PostsController < ApplicationController
+    def index
+      # Placeholder action
+    end
+  
+    def show
+      # Placeholder action
+    end
+  end
+  
